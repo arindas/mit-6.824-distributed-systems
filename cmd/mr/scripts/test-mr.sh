a94ac847f370fcc97683617d324a682e0abda610
Arindam Das <dasarindam.mails@gmail.com>
2022-01-06 19:16:41 +0530

    Corrected mrapps path in script.

--- cmd/mr/scripts/test-mr.sh
+++ cmd/mr/scripts/test-mr.sh
@@ -16,14 +16,14 @@ cd mr-tmp || exit 1
 rm -f mr-*
 
 # make sure software is freshly built.
-(cd ../../mrapps && go build $RACE -buildmode=plugin wc.go) || exit 1
-(cd ../../mrapps && go build $RACE -buildmode=plugin indexer.go) || exit 1
-(cd ../../mrapps && go build $RACE -buildmode=plugin mtiming.go) || exit 1
-(cd ../../mrapps && go build $RACE -buildmode=plugin rtiming.go) || exit 1
-(cd ../../mrapps && go build $RACE -buildmode=plugin jobcount.go) || exit 1
-(cd ../../mrapps && go build $RACE -buildmode=plugin early_exit.go) || exit 1
-(cd ../../mrapps && go build $RACE -buildmode=plugin crash.go) || exit 1
-(cd ../../mrapps && go build $RACE -buildmode=plugin nocrash.go) || exit 1
+(cd ../apps && go build $RACE -buildmode=plugin wc.go) || exit 1
+(cd ../apps && go build $RACE -buildmode=plugin indexer.go) || exit 1
+(cd ../apps && go build $RACE -buildmode=plugin mtiming.go) || exit 1
+(cd ../apps && go build $RACE -buildmode=plugin rtiming.go) || exit 1
+(cd ../apps && go build $RACE -buildmode=plugin jobcount.go) || exit 1
+(cd ../apps && go build $RACE -buildmode=plugin early_exit.go) || exit 1
+(cd ../apps && go build $RACE -buildmode=plugin crash.go) || exit 1
+(cd ../apps && go build $RACE -buildmode=plugin nocrash.go) || exit 1
 (cd .. && go build $RACE mrcoordinator.go) || exit 1
 (cd .. && go build $RACE mrworker.go) || exit 1
 (cd .. && go build $RACE mrsequential.go) || exit 1
